4236145ec3b127bb88add978d26c527b57454476
00King00 <mr.1bilous@gmail.com>
2019-02-13 16:50:27 +0200

    add vuetify and pug settting

--- src/main.js
+++ src/main.js
@@ -2,6 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify, {
+	VApp, // required
+	VNavigationDrawer,
+	VToolbar,
+	VFadeTransition,
+	VCard,
+	VList
+} from 'vuetify/lib'
+
+import { Ripple } from 'vuetify/lib/directives'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VToolbar,
+    VFadeTransition,
+    VCard,
+    VList
+  },
+  directives: {
+    Ripple
+  }
+});
 
 Vue.config.productionTip = false
 
