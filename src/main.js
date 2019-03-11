ef7f4a9957eb8dcd2c4368fc675215353492439e
00King00 <mr.1bilous@gmail.com>
2019-03-11 21:33:39 +0200

    finish data-language

--- src/main.js
+++ src/main.js
@@ -4,86 +4,89 @@ import router from './router'
 import store from './store'
 import 'vuetify/dist/vuetify.min.css'
 import '@/assets/sass/main.sass'
-import Vuetify, {
-	VApp, // required
-	VNavigationDrawer,
-	VToolbar,
-	VFadeTransition,
-	VCard,
-	VHover,
-	VList,
-	VListGroup,
-	VListTile,
-	VListTileTitle,
-	VListTileAction,
-	VListTileContent,
-	VIcon,
-	VDivider,
-	VListTileAvatar,
-	VBadge,
-	VMenu,
-	VDialog,
-	VContent,
-	VContainer,
-	VBtn,
-	VSpacer,
-	VLayout,
-	VFooter,
-	VFlex,
-	VCardTitle,
-	VTextField,
-	VCardActions,
-	VToolbarSideIcon,
-	VToolbarTitle,
-	VCardText,
-	VToolbarItems,
-	VForm,
-	VCheckbox
-} from 'vuetify/lib'
+import Vuetify from 'vuetify'
+ Vue.use(Vuetify)
 
-import { Ripple } from 'vuetify/lib/directives'
-
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VNavigationDrawer,
-    VToolbar,
-    VFadeTransition,
-    VCard,
-	VHover,
-    VList,
-	VListGroup,
-	VListTile,
-	VListTileTitle,
-	VListTileAction,
-	VListTileContent,
-	VIcon,
-	VDivider,
-	VListTileAvatar,
-	VBadge,
-	VMenu,
-	VDialog,
-	VContent,
-	VContainer,
-	VBtn,
-	VSpacer,
-	VLayout,
-	VFooter,
-	VFlex,
-	VCardTitle,
-	VTextField,
-	VCardActions,
-	VToolbarSideIcon,
-	VToolbarTitle,
-	VCardText,
-	VToolbarItems,
-	VForm,
-	VCheckbox
-  },
-  directives: {
-    Ripple
-  }
-});
+// import Vuetify, {
+// 	VApp, // required
+// 	VNavigationDrawer,
+// 	VToolbar,
+// 	VFadeTransition,
+// 	VCard,
+// 	VHover,
+// 	VList,
+// 	VListGroup,
+// 	VListTile,
+// 	VListTileTitle,
+// 	VListTileAction,
+// 	VListTileContent,
+// 	VIcon,
+// 	VDivider,
+// 	VListTileAvatar,
+// 	VBadge,
+// 	VMenu,
+// 	VDialog,
+// 	VContent,
+// 	VContainer,
+// 	VBtn,
+// 	VSpacer,
+// 	VLayout,
+// 	VFooter,
+// 	VFlex,
+// 	VCardTitle,
+// 	VTextField,
+// 	VCardActions,
+// 	VToolbarSideIcon,
+// 	VToolbarTitle,
+// 	VCardText,
+// 	VToolbarItems,
+// 	VForm,
+// 	VCheckbox
+// } from 'vuetify/lib'
+//
+// import { Ripple } from 'vuetify/lib/directives'
+//
+// Vue.use(Vuetify, {
+//   components: {
+//     VApp,
+//     VNavigationDrawer,
+//     VToolbar,
+//     VFadeTransition,
+//     VCard,
+// 	VHover,
+//     VList,
+// 	VListGroup,
+// 	VListTile,
+// 	VListTileTitle,
+// 	VListTileAction,
+// 	VListTileContent,
+// 	VIcon,
+// 	VDivider,
+// 	VListTileAvatar,
+// 	VBadge,
+// 	VMenu,
+// 	VDialog,
+// 	VContent,
+// 	VContainer,
+// 	VBtn,
+// 	VSpacer,
+// 	VLayout,
+// 	VFooter,
+// 	VFlex,
+// 	VCardTitle,
+// 	VTextField,
+// 	VCardActions,
+// 	VToolbarSideIcon,
+// 	VToolbarTitle,
+// 	VCardText,
+// 	VToolbarItems,
+// 	VForm,
+// 	VCheckbox
+//   },
+//   directives: {
+//     Ripple
+//   }
+// });
 
 Vue.config.productionTip = false
 
